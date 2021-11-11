2c8ae9305d2266fe8e32d1e545af23937fc84fe7
Kolenkaa <kkolenkaa@gmail.com>
2021-11-11 15:31:14 +0300

    14

--- js/script.js
+++ js/script.js
@@ -1,11 +1,3 @@
-/*--
-$(document).ready(function() {
-	$('.burger').click(function(event) {
-		$('.burger,.menu').toggleClass('active')
-	});
-});
-	--*/
-
 document.addEventListener('DOMContentLoaded', function () {
 	document.querySelector('.burger').onclick = function (event) {
 		document.querySelector('.burger').classList.toggle('active');
