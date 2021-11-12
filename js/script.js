f8df76bc30cfeb7cd8dac5af0bb2e3c15fceb013
Kolenkaa <kkolenkaa@gmail.com>
2021-11-12 11:02:25 +0300

    fix changes @media for .container

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const burger = document.querySelector(".burger");
-  const mobileMenu = document.querySelector(".mobile-menu");
+	const burger = document.querySelector(".burger");
+	const mobileMenu = document.querySelector(".mobile-menu");
 
-  burger.onclick = function (event) {
-    burger.classList.toggle("active");
-    mobileMenu.classList.toggle("active");
-  };
+	burger.onclick = function (event) {
+		burger.classList.toggle("active");
+		mobileMenu.classList.toggle("active");
+	};
 });
