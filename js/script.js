7fd406cd79d96390b6a2d7e1a5de1afbd335e18c
Kolenkaa <kkolenkaa@gmail.com>
2021-12-08 14:04:01 +0300

    correcting the code

--- js/script.js
+++ js/script.js
@@ -7,3 +7,24 @@ document.addEventListener("DOMContentLoaded", function () {
 		mobileMenu.classList.toggle("active");
 	};
 });
+
+/*----------------Team__Slider-------------*/
+let offset = 0; // смещение от левого края
+const sliderInner = document.querySelector('.team__photo-inner');
+
+document.querySelector('.team__slider-button--right').addEventListener('click', function () {
+	offset = offset + 360;// offset += 360;
+	if (offset > 720) {
+		offset = 0;
+	}
+	sliderInner.style.left = -offset + 'px';
+})
+
+document.querySelector('.team__slider-button--left').addEventListener('click', function () {
+	offset = offset - 360;// offset -= 360;
+	if (offset < 0) {
+		offset = 720;
+	}
+	sliderInner.style.left = -offset + 'px';
+})
+
